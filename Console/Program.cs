8bb2df1f085e7170daf4bdf7b871336b89dca157
Matimba-codes <kubayimt@gmail.com>
2024-01-20 18:40:07 +0200

    File upload of all codes

---
+++ Console/Program.cs
@@ -0,0 +1,69 @@
+﻿class Campuses
+{
+    static void Main(string[] args)
+    {
+        question2_1();
+
+        question2_2();
+    }
+
+    private static void question2_1()
+    {
+        string[] campuses = {"Midrand", "Pretoria", "Bedfordview", "Vanderbijlpark", "Potchefstroom", "Durban", "East London", "Nelson Mandela Bay", "Bloemfontein", "Mbombela", "Tyger valley", "Claremont"};
+
+        Console.WriteLine("Eduvos Campuses List");
+        for (int i = 0; i < campuses.Length; i++)
+        {
+            Console.WriteLine(campuses[i]);
+        }
+
+        Console.Write("\n");
+        Console.WriteLine("Eduvos Campuses grouped together:");
+        Console.Write("\n");
+
+        var result = from i in Enumerable.Range(0, campuses.Length)
+                     group campuses[i] by i / 3;
+
+        foreach (var campusesGroup in result)
+        {
+            foreach (var campus in campusesGroup)
+            {
+                Console.Write($"{campus}    ");
+            }
+            Console.Write("\n");
+        }
+    }
+
+    private static void question2_2()
+    {
+        SortedDictionary<string, string> campusDictionary = new SortedDictionary<string, string>();
+
+        campusDictionary.Add("Midrand", "BSc Computer Science");
+        campusDictionary.Add("Pretoria", "BSc IT in Robotics");
+        campusDictionary.Add("Bedfordview", "BSc IT in Software Engineering");
+        campusDictionary.Add("Vanderbijlpark", "BSc IT in Software Engineering");
+        campusDictionary.Add("Potchefstroom", "BSc IT in Software Engineering");
+        campusDictionary.Add("Durban", "BSc IT in Robotics");
+        campusDictionary.Add("East London", "BSc in Data Science");
+        campusDictionary.Add("Nelson Mandela Bay", "BSc IT in Security and Network Engineering");
+        campusDictionary.Add("Bloemfontein", "BSc in Data Science");
+        campusDictionary.Add("Mbombela", "BSc in Data Science");
+        campusDictionary.Add("Tyger valley", "BSc IT in Robotics");
+        campusDictionary.Add("Claremont", "BSc IT in Security and Network Engineering");
+
+        foreach (KeyValuePair<string, string> pair in campusDictionary)
+        {
+            Console.WriteLine($"{pair.Key}, {pair.Value}");
+        }
+
+        Console.WriteLine($"The totalnumber of campuses of Eduvos is: {campusDictionary.Count}");
+
+        int i = 1;
+        if (campusDictionary.ContainsValue("BSc IT in Software Engineering"))
+        {            
+            i++;
+        }
+
+        Console.WriteLine($"Number of campuses offering BSc IT in Software Engineering is: {i}");
+    }
+}
---
+++ Form/Form1.cs
@@ -0,0 +1,56 @@
+namespace Matimba_Kubayi_8LC3G3SF9_ITPCA2_B12_Project_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\kubay\Documents\University\BSC IT\ITPCA2-12\Block 2\MedicineList.txt";
+            checkedListBox1.Items.AddRange(System.IO.File.ReadAllLines(filePath));
+
+            Random number = new Random();
+            string[] word = File.ReadAllLines(@"C:\Users\kubay\Documents\University\BSC IT\ITPCA2-12\Block 2\MedicineList.txt");
+            label4.Text = $"Special: {number.Next(1, 50)}% discount on {word[new Random().Next(word.Length)]}";
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter writer = new StreamWriter(@"C:\Users\kubay\Documents\University\BSC IT\ITPCA2-12\Block 2\OrderList.txt");
+            writer.WriteLine("Order form:\n",checkedListBox1.CheckedItems);
+
+            MessageBox.Show("Order Saved", "Message", MessageBoxButtons.OK);
+
+            Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.Items.Contains(textBox1.Text))
+            {
+                MessageBox.Show("That drug already exists in the list", "Error", MessageBoxButtons.OK);
+            }
+            else if (textBox1.Text=="")
+            {
+                MessageBox.Show("Please enter a new medicine", "Error", MessageBoxButtons.OK);
+            }
+            else
+            {
+                checkedListBox1.Items.Add(textBox1.Text);
+            }
+        }
+    }
+}
